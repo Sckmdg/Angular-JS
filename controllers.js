a2454b232b6e7f031216da834302e21da73fb2f5
Sckmdg <archer1strong@gmail.com>
2016-12-09 13:02:13 +0600

    Add files via upload

---
+++ controllers.js
@@ -0,0 +1,42 @@
+'use strict';
+var App = angular.module('App', []);
+App.controller('ListCtrl', function($scope) {
+	$scope.users = [
+	{"username":"user1","firstname":"John","lastname":"Anderson","age":25,"email":"john.anderson@testtask.com"},
+	{"username":"user2","firstname":"Paul","lastname":"Winfred","age":27,"email":"winfredMPaul@dayrep.com"},
+	{"username":"user3","firstname":"Adan","lastname":"Kelley","age":64,"email":"adanMKelley@jourrapide.com"},
+	{"username":"user4","firstname":"Vernon","lastname":"McCall","age":70,"email":"vernonKMcCall@teleworm.us"},
+	{"username":"user5","firstname":"Ernest","lastname":"Heflin","age":38,"email":"ernestMHeflin@jourrapide.com"},
+	{"username":"user6","firstname":"John","lastname":"Barton","age":52,"email":"JohnPBarton@jourrapide.com"},
+	{"username":"user7","firstname":"Jessica","lastname":"Lara","age":25,"email":"JessicaRLara@jourrapide.com"}
+	];
+
+	$scope.addRow = function(user){		
+		$scope.users.push(user);
+	};
+
+	$scope.removeRow = function(username){				
+		var index = -1;		
+		var comArr = eval( $scope.users );
+		for( var i = 0; i < comArr.length; i++ ) {
+			if( comArr[i].username === username ) {
+				index = i;
+				break;
+			}
+		}
+		if( index === -1 ) {
+			alert( "Something gone wrong" );
+		}
+		$scope.users.splice( index, 1 );		
+	};	
+
+	$scope.edit = function(user){
+		$scope.current = user;
+	};
+
+	$scope.save = function(user){
+		$scope.current = {};
+	};
+
+	$scope.current = {};
+});
